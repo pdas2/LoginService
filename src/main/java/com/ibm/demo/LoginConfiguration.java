f0d37e5a6a1c0a0abbf0fb3ebe66b71d52ddd2c1
pdas2 <pdas2@in.ibm.com>
2017-12-20 16:39:36 +0530

    logback changes added

---
+++ src/main/java/com/ibm/demo/LoginConfiguration.java
@@ -0,0 +1,18 @@
+package com.ibm.demo;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/*
+@Configuration
+@ComponentScan("com.ibm.demo")
+
+public class LoginConfiguration {
+
+	@Bean
+	public AlwaysSampler defaultSampler() {
+	  return new AlwaysSampler();
+	}
+}
+*/
